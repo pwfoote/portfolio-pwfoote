d9ef8dce5de324fc61d680d4baf3235aa842876d
prestonwfoote <prestonwfoote@gmail.com>
2022-03-14 23:53:01 -0400

    add bootstrap and color

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import Profile from '../../assets/projectImage/cover-image.jpg'
 //import picture from the previous passport. create a "assets" directory to save it as indicated below
 //import profilePicture from '../../assets/profile/profile-picture.jpg';
 //<img src={profilePicture} className="my-2" style={{ width: "100%" }} alt="profile" />
 function About() {
     return(
         <section id="about">
-        <div>
+        
             <h1> About me </h1>
 
+
+            <div id="wrapper">
+               <div className="m-2" id="profile-picture">
+                   <img 
+                   src = {Profile}
+                   alt="profile picture"
+                   >
+                   </img>
+              </div>
+
         <p>
-            Preston Foote 
+            BIO TEXT
         </p>
 
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,10 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+  crossorigin="anonymous"
+/>
